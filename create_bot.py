34938a5607c036dd6720b6292a58998032405a3a
PomergranateFan <104011529+PomergranateFan@users.noreply.github.com>
2023-05-03 20:34:00 +0300

    Update create_bot.py

--- create_bot.py
+++ create_bot.py
@@ -2,4 +2,4 @@ from aiogram import Bot
 from aiogram.dispatcher import Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
-bot = Bot(token="5944101199:AAH-jMfwm8rO-mLVd5tc3rI75wN5R3hgnzk")
+bot = Bot(token="")
